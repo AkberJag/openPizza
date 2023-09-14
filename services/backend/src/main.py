c457bc059779bc4ecad110ba260434574cae4cfb
Akber <akberjag@gmail.com>
2023-09-14 05:07:44 +0100

    :tada: feat: Initialize main.py

---
+++ services/backend/src/main.py
@@ -0,0 +1,34 @@
+"""Main application package containing the app factory function."""
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.api import api_v1
+
+
+def create_application() -> FastAPI:
+    """Create application factory"""
+    application = FastAPI()
+    include_routers(application)
+    include_middleware(application)
+    return application
+
+
+def include_routers(application: FastAPI) -> None:
+    """Include FastAPI routers."""
+    application.include_router(api_v1.api_v1_router)
+
+
+def include_middleware(application: FastAPI) -> None:
+    """Include Middlewares"""
+    origins = ["http://localhost:8080"]
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_headers=["*"],
+    )
+
+
+app = create_application()
