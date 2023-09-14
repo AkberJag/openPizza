fc4fe8bf25e1ebd648121b9cf82165054f2199b3
Akber <akberjag@gmail.com>
2023-09-14 21:16:42 +0100

    :sparkles: feat(api): Add login endpoint

--- services/backend/src/api/api_v1/endpoints/auth.py
+++ services/backend/src/api/api_v1/endpoints/auth.py
@@ -1,6 +1,10 @@
 """auth routes"""
 
+from typing import Annotated
+
 from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 
@@ -8,6 +12,9 @@ from src.crud import crud_user
 from src.schemas import UserCreate, UserInDB
 from src.database.session import get_db
 from src.auth.security import get_password_hash
+from src.auth.dependencies import get_current_user
+from src.models import User
+from src.auth.jwthandler import create_access_token
 
 router = APIRouter()
 
@@ -26,3 +33,33 @@ async def create_user(user_in: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail="This email is already in use"
         ) from exc
+
+
+@router.post("/login")
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    db_user = crud_user.authenticate(
+        db, email=form_data.username, password=form_data.password
+    )
+
+    token = create_access_token(user=db_user)
+
+    content = {
+        "username": db_user.fullname,
+        "message": "You've successfully logged in. Welcome back!",
+        "access_token": token,
+        "token_type": "bearer",
+    }
+    response = JSONResponse(content=content)
+    response.set_cookie(
+        "access_token",
+        value=f"Bearer {token}",
+        httponly=True,
+        max_age=1800,
+        expires=1800,
+        samesite="Lax",
+        secure=False,
+    )
+
+    return response
