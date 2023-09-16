72eac093dd24e97280ae9fc0f38b26f78ccb5578
Akber <akberjag@gmail.com>
2023-09-16 02:32:55 +0100

    🔧feat(vite): Configure server settings in Vite config
    this to expose the port 8080 from the container to the outer world

--- services/frontend/vite.config.js
+++ services/frontend/vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 8080
+  },
   plugins: [
     vue(),
   ],
