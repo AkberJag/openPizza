27a6eb24b02e45a195c65e165a0ab5b62f379631
Akber <akberjag@gmail.com>
2023-09-16 03:33:17 +0100

    :sparkles: feat(router): Add routes constant to router with home path

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -1,7 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router"
 
+const routes = [{
+    path: '/',
+    component: HomePage,
+    name: "Home"
+}]
+
 const router = createRouter({
     history: createWebHistory(),
+    routes
 })
 
 export default router;
