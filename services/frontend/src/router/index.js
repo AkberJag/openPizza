6e0b5850a1d8ab0fd202353e0d69d8b3022f8b6a
Akber <akberjag@gmail.com>
2023-09-17 19:03:29 +0100

    :sparkles: feat(login): create login page

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -1,16 +1,24 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/Home/HomePage.vue'
+import UserLogin from '@/views/Auth/UserLogin.vue'
 
-const routes = [{
-    path: '/',
-    component: HomePage,
-    name: "Home"
-}]
+const routes = [
+    {
+        path: '/',
+        component: UserLogin,
+        name: 'Home'
+    },
+    {
+        path: '/user/login',
+        component: UserLogin,
+        name: 'Login'
+    }
+]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
 
-export default router;
+export default router
---
+++ services/frontend/src/views/auth/UserLogin.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="bg-body-tertiary w-100 body">
+    <main class="form-signin w-100 m-auto">
+      <div class="centered-container">
+        <form class="centered-form">
+          <p style="font-size: 8cap">🍕</p>
+          <h1 class="h3 mb-3 fw-normal">Please Login</h1>
+
+          <div class="form-floating">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address / User Name</label>
+          </div>
+          <div class="form-floating">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+
+          <div class="form-check text-start my-3">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              value="remember-me"
+              id="flexCheckDefault"
+            />
+            <label class="form-check-label" for="flexCheckDefault">Remember me </label>
+          </div>
+          <button class="btn btn-primary w-100 py-2" type="submit">Log in</button>
+        </form>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.body {
+  position: fixed;
+  left: 0;
+  height: 100%;
+}
+
+.centered-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+}
+
+.centered-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100px;
+  width: 100%;
+}
+
+.form-floating {
+  width: 100%;
+}
+.form-signin {
+  max-width: 330px;
+  padding: 1rem;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type='email'] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type='password'] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
