576c5692fa437a352a81fbb03073bbc3e3356751
Akber <akberjag@gmail.com>
2023-09-17 22:18:54 +0100

    :package: feat(state-management): add Vuex

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,12 +1,14 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-icons/font/bootstrap-icons.css"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store/index.js'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
