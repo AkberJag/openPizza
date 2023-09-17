40bce7fff0b6c268adb64e10bbfa9ad965752008
Akber <akberjag@gmail.com>
2023-09-17 21:12:19 +0100

    :sparkles: feat(popover): enable popover
    import "bootstrap" is not compatible with popover so only importing of bootstrap.css is enough

--- services/frontend/src/App.vue
+++ services/frontend/src/App.vue
@@ -1,3 +1,20 @@
 <template>
   <router-view></router-view>
 </template>
+
+<script>
+import { Popover } from 'bootstrap/dist/js/bootstrap.esm.min.js'
+// import { Toast } from 'bootstrap/dist/js/bootstrap.esm.min.js'
+
+export default {
+  mounted() {
+    // enable popover
+    Array.from(document.querySelectorAll('button[data-bs-toggle="popover"]')).forEach(
+      (popoverNode) => new Popover(popoverNode)
+    )
+
+    // //inti toast
+    // Array.from(document.querySelectorAll('.toast')).forEach((toastNode) => new Toast(toastNode))
+  }
+}
+</script>
--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 import { createApp } from 'vue'
