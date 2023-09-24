bc4ac1ff211ce40b771617252edc88594e4419f1
Akber <akberjag@gmail.com>
2023-09-24 17:02:02 +0100

    :lipstick: feat(components): add base alert container component

---
+++ services/frontend/src/components/layout/TheAlertContainer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container text-center my-1" style="max-height: 150px; overflow-y: auto">
+    <base-alert
+      v-for="(alert, index) in alerts"
+      :key="index"
+      :alertID="alert.id.toString()"
+      :message="alert.message"
+      :strongMessage="alert.strongMessage"
+    ></base-alert>
+  </div>
+</template>
+
+<script>
+import BaseAlert from '@/components/ui/BaseAlert.vue'
+export default {
+  components: { BaseAlert },
+
+  data() {
+    return {
+      alerts: []
+    }
+  },
+  methods: {
+    addAlert() {
+      this.alerts.push({
+        id: Date.now(),
+        message:
+          'A simple secondary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.',
+        strongMessage: 'Too strong'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  z-index: 2000;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+}
+</style>
