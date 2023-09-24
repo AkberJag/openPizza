905590aa9972d65853b11ddc5b547452ef9c349e
Akber <akberjag@gmail.com>
2023-09-24 17:03:09 +0100

    :sparkles: feat(App.vue): add alert component

--- services/frontend/src/App.vue
+++ services/frontend/src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <TheAlertContainer />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
 import { Popover } from 'bootstrap/dist/js/bootstrap.esm.min.js'
 // import { Toast } from 'bootstrap/dist/js/bootstrap.esm.min.js'
+import TheAlertContainer from '@/components/layout/TheAlertContainer.vue'
 
 export default {
+  components: { TheAlertContainer },
   mounted() {
     // enable popover
     Array.from(document.querySelectorAll('button[data-bs-toggle="popover"]')).forEach(
       (popoverNode) => new Popover(popoverNode)
     )
-
-    // //inti toast
+    //inti toast
     // Array.from(document.querySelectorAll('.toast')).forEach((toastNode) => new Toast(toastNode))
   }
 }
