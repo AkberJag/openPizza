5276f1b1b5b0dc6f77833092ae06fbed9d6ab9f8
AkberJag <akberjag@gmail.com>
2023-10-08 00:15:10 +0100

    :tada: feat(components/home): add bill pane component

---
+++ services/frontend/src/components/home/BillPane.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-100">
+    <div class="card shadow d-flex">
+      <div class="card-header">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4 text-start p-1 text-body-secondary">
+              <i class="bi bi-printer"> Print</i>
+            </div>
+            <div class="col-md-4 text-center fw-bold fs-4">#9</div>
+            <div class="col-md-4 text-end p-1">
+              <div class="d-inline mx-3 text-body-secondary"><i class="bi bi-copy"></i> Copy</div>
+              <div class="d-inline text-body-secondary"><i class="bi bi-trash"></i> Clear</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
