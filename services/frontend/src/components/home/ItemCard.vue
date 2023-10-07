6b7b4756d1199d90d15c37ef4e20fa3688183049
AkberJag <akberjag@gmail.com>
2023-10-07 21:15:26 +0100

    :sparkles: feat(components): add item card component

---
+++ services/frontend/src/components/home/ItemCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card m-3 shadow-sm" style="max-height: 60px">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img :src="imageURL" class="img-fluid rounded-start" alt="" />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <p class="card-text">{{ title }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['imageURL', 'title']
+}
+</script>
+
+<style scoped>
+img {
+  height: 58px;
+}
+</style>
