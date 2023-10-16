b57be2684bed4ba4f853e6b097c9e0bebc49fcd0
AkberJag <akberjag@gmail.com>
2023-10-17 00:24:31 +0100

    :rocket: feat(routes): added route to retrieve all FoodCategories

--- services/backend/src/api/api_v1/endpoints/home/router.py
+++ services/backend/src/api/api_v1/endpoints/home/router.py
@@ -1,13 +1,18 @@
-from typing import Annotated
-
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from src.models import User
+from src.database.session import get_db
 from src.auth.dependencies import get_current_user
+from src.crud.food import crud_food_category
+from src.schemas.food import FoodCategory as FoodCategory_schema
 
 router = APIRouter()
 
 
-@router.get("/")
-async def home(current_user: Annotated[User, Depends(get_current_user)]):
-    return f"You have access {current_user}"
+@router.get(
+    "/food_categories",
+    dependencies=[Depends(get_current_user)],
+    response_model=list[FoodCategory_schema],
+)
+async def food_categories(db: Session = Depends(get_db)):
+    return crud_food_category.get_multi(db)
