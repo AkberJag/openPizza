3efe65716bd46b75e2c3eb985f7dd62c96e60432
AkberJag <akberjag@gmail.com>
2023-10-17 00:41:42 +0100

    :lock: feat(database): make food items and category names unique

--- services/backend/src/models/food.py
+++ services/backend/src/models/food.py
@@ -13,7 +13,7 @@ class FoodCategory(Base):
     __tablename__ = "food_categories"
 
     category_id = Column(Integer, primary_key=True, index=True)
-    category_name = Column(String, index=True)
+    category_name = Column(String, index=True, unique=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     # Relationship to FoodItem
@@ -26,7 +26,7 @@ class FoodItem(Base):
     __tablename__ = "food_items"
 
     item_id = Column(Integer, primary_key=True, index=True)
-    item_name = Column(String, index=True)
+    item_name = Column(String, index=True, unique=True)
     category_id = Column(Integer, ForeignKey("food_categories.category_id"))
     price = Column(Float)
     image_url = Column(String)
