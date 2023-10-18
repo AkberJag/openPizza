f7b251b7c2eb605a61484957a4b2f5527039efab
AkberJag <akberjag@gmail.com>
2023-10-18 22:15:57 +0100

    :rocket: feat(api): added route to get food items by category

--- services/backend/src/api/api_v1/endpoints/home/router.py
+++ services/backend/src/api/api_v1/endpoints/home/router.py
@@ -4,9 +4,9 @@ from sqlalchemy.exc import IntegrityError
 
 from src.database.session import get_db
 from src.auth.dependencies import get_current_user
-from src.crud.food import crud_food_category
+from src.crud.food import crud_food_category, crud_food_item
 from src.schemas.food import FoodCategory as FoodCategory_schema
-from src.schemas.food import FoodCategoryCreate
+from src.schemas.food import FoodCategoryCreate, FoodItem, FoodItemCreate
 
 router = APIRouter()
 
@@ -38,5 +38,22 @@ async def add_food_category(
 
     except Exception as e:
         raise HTTPException(
-            status_code=500, detail=f"Internal server error: {str(e)}"
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Internal server error: {str(e)}",
         ) from e
+
+
+@router.get(
+    "/food_category/{category_id}/food_items",
+    response_model=list[FoodItem],
+)
+async def food_items_by_category(category_id: int, db: Session = Depends(get_db)):
+    food_items = crud_food_item.get_food_items_by_category(db, category_id=category_id)
+
+    if not food_items:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No food items found in this category",
+        )
+
+    return food_items
