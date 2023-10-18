ad0fbcb20d1332baba638e2275716d89d4993ea1
AkberJag <akberjag@gmail.com>
2023-10-18 17:38:01 +0100

    :sparkles: feat(models): add FoodItem Pydantic model  In this commit message:      :sparkles: is a custom Gitmoji used to indicate the addition of new features.     feat(models) specifies that the change is related to models.     "add FoodItem Pydantic model" provides a clear and concise description of the change, indicating that you've added a Pydantic model for the FoodItem.

--- services/backend/src/schemas/food.py
+++ services/backend/src/schemas/food.py
@@ -36,3 +36,47 @@ class FoodCategoryInDB(FoodCategoryInDBBase):
     """Additional properties stored in DB"""
 
     created_at: datetime
+
+
+# Food Items
+class FoodItemBase(BaseModel):
+    """shared properties"""
+
+    item_name: None | str = None
+    price: None | float = None
+    image_url: None | HttpUrl = None
+
+
+class FoodItemCreate(FoodItemBase):
+    """Properties to receive via API on creation"""
+
+    item_name: str
+    price: float
+
+    category_id: int
+
+
+class FoodItemUpdate(FoodItemBase):
+    """Properties to receive via API on update"""
+
+
+# Food Items - in DB
+class FoodItemInDBBase(FoodItemBase):
+    """Shared properties - db"""
+
+    item_id: int
+
+    class Config:
+        from_attributes = True
+
+
+class FoodItem(FoodItemInDBBase):
+    """Additional properties to return via API"""
+
+    category_id: int
+
+
+class FoodItemInDB(FoodItemInDBBase):
+    """Additional properties stored in DB"""
+
+    created_at: datetime
