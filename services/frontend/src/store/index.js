8fbffbe39462c6e9fa67f066dd48d42f42e272ac
AkberJag <akberjag@gmail.com>
2023-10-20 20:44:26 +0100

    :sparkles: feat(store/global): added food module

--- services/frontend/src/store/index.js
+++ services/frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from 'vuex'
 import authModule from '@/store/modules/auth/index.js'
 import globalModule from '@/store/modules/global/index.js'
+import foodDataModule from '@/store/modules/food/index.js'
 
 const store = createStore({
   modules: {
     auth: authModule,
-    global: globalModule
+    global: globalModule,
+    foodData: foodDataModule
   }
 })
 
