31b5e27f72d4c9bf32ceb5b06275d060c62e99bc
AkberJag <akberjag@gmail.com>
2023-10-22 22:40:05 +0100

    :recycle: refactor(components): updated bill item component to accept props from parent

--- services/frontend/src/components/home/BillItem.vue
+++ services/frontend/src/components/home/BillItem.vue
@@ -1,23 +1,23 @@
 <template>
   <tr>
-    <th scope="row" class="fs-6">1</th>
-    <td>
+    <th scope="row" class="fs-6">{{ count + 1 }}</th>
+    <td class="col-11">
       <div class="row me-0">
         <div class="fw-bold text-capitalize fs-6 col-10 m-0" style="cursor: pointer" @click="test">
-          chicken francese
+          {{ item.item_name }}
           <span class="right-icon ms-auto" :class="{ 'right-icon-rotate': rotateArrow }">
             <i class="bi bi-chevron-down"></i>
           </span>
         </div>
-        <div class="fw-bold text-end col-2">$12</div>
+        <div class="fw-bold text-end col-2">${{ item.price }}</div>
       </div>
-      <div class="collapse m-0" :id="myclass">
+      <div class="collapse m-0" :id="count">
         <div class="row me-0">
           <div class="col-10">
             <span class="me-2 text-danger" style="opacity: 75%">Type</span>
             <span class="text-body-secondary">Dinner</span>
           </div>
-          <div class="col-2 text-end text-body-secondary">$12</div>
+          <div class="col-2 text-end text-body-secondary">${{ item.price }}</div>
         </div>
       </div>
     </td>
@@ -27,11 +27,10 @@
 <script>
 import { Collapse } from 'bootstrap/dist/js/bootstrap.esm.min.js'
 export default {
-  props: ['myclass'],
+  props: ['count', 'item'],
 
   mounted() {
-    const collapseElement = document.getElementById(this.myclass)
-
+    const collapseElement = document.getElementById(this.count)
     this.collapseInstance = new Collapse(collapseElement, { toggle: false })
   },
 
