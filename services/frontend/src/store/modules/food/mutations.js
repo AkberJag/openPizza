d54c38ac869bac73609bb0aeec18e051b4361e9f
AkberJag <akberjag@gmail.com>
2023-10-22 19:45:31 +0100

    :sparkles: feat(store/food): added mutation to set food items

--- services/frontend/src/store/modules/food/mutations.js
+++ services/frontend/src/store/modules/food/mutations.js
@@ -1,5 +1,11 @@
 export default {
   addCategory(state, payload) {
     state.menuData = payload
+  },
+  setItems(state, payload) {
+    const categoryIndex = state.menuData.findIndex((category) => category.categoryID === payload.id)
+    if (categoryIndex !== -1) {
+      state.menuData[categoryIndex].items = payload.data
+    }
   }
 }
