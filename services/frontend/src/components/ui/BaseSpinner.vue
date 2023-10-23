dba0d982be92c0b492f4381b47c957da05a482ee
AkberJag <akberjag@gmail.com>
2023-10-23 02:02:26 +0100

    :sparkles: feat(components): added universal loading spinner

---
+++ services/frontend/src/components/ui/BaseSpinner.vue
@@ -0,0 +1,29 @@
+<!-- <template><span class="loader"></span></template>
+
+<style scoped>
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 5px solid #0059ff;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style> -->
+
+<template>
+  <div class="spinner-border text-primary" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</template>
--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -6,9 +6,13 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store/index.js'
 
+import BaseSpinner from '@/components/ui/BaseSpinner.vue'
+
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+
+app.component('base-spinner', BaseSpinner)
 
 app.mount('#app')
