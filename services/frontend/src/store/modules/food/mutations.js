3286662e5dca5a132b9da069fe99021acf4113c1
AkberJag <akberjag@gmail.com>
2023-10-26 14:27:33 +0100

    :sparkles: feat(store/food): implement item quantity check before adding

--- services/frontend/src/store/modules/food/mutations.js
+++ services/frontend/src/store/modules/food/mutations.js
@@ -10,6 +10,13 @@ export default {
   },
   addItemTocurrentCart(state, payload) {
     state.currentCart.subTotal += payload.price
-    state.currentCart.items.push(payload)
+    const itemIndex = state.currentCart.items.findIndex((item) => item.item_id === payload.item_id)
+    if (itemIndex !== -1) {
+      // If the item exists, increase its qty
+      state.currentCart.items[itemIndex].qty++
+    } else {
+      // If the item doesn't exist, add it to the array with qty 1
+      state.currentCart.items.push({ ...payload, qty: 1 })
+    }
   }
 }
