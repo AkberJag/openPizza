e3b7dfc16984bd2c6e29b5db46f93331f301253f
AkberJag <akberjag@gmail.com>
2023-11-16 00:14:52 +0000

    :sparkles: feat(components): Added toast container component to hold all toasts

---
+++ services/frontend/src/components/layout/TheToastContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="toast-container position-fixed bottom-0 end-0 p-2">
+    <BaseToast
+      v-for="(toast, index) in toasts"
+      :key="index"
+      :id="Date.now().toString()"
+      :toastTitle="toast.toastTitle"
+      :toastBody="toast.toastBody"
+      :toastImgURL="toast.toastImgURL"
+    />
+  </div>
+</template>
+
+<script>
+import BaseToast from '@/components/ui/BaseToast.vue'
+export default {
+  components: { BaseToast },
+
+  data() {
+    return {
+      toasts: this.$store.state.global.toasts
+    }
+  }
+}
+</script>
+
+<style scoped>
+.toast-container {
+  max-height: 500px;
+  overflow: auto;
+}
+</style>
