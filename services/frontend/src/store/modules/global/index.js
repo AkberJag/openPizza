a79eabfe3bff2ba0a1f046a497544993649b0365
AkberJag <akberjag@gmail.com>
2023-11-16 00:16:59 +0000

    :sparkles: feat(store/global): Added 'toasts' store to hold all toasts

--- services/frontend/src/store/modules/global/index.js
+++ services/frontend/src/store/modules/global/index.js
@@ -5,6 +5,7 @@ export default {
   state() {
     return {
       errors: [{ message: 'test', strongMessage: 'St' }],
+      toasts: [{ toastTitle: 'Test toast', toastBody: 'This is a test toast' }],
       theme: localStorage.getItem('theme') || 'light'
     }
   },
