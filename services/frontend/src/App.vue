d6100866659758dba767cc22d817246a7b71d545
AkberJag <akberjag@gmail.com>
2023-11-16 00:15:56 +0000

    :sparkles: feat(App): Added global toast container

--- services/frontend/src/App.vue
+++ services/frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <TheAlertContainer />
+    <TheToastContainer />
     <div class="container-fluid">
       <div class="row flex-nowrap no-gutters">
         <div v-if="true" class="col-auto p-0">
@@ -22,12 +23,13 @@
 
 <script>
 import { Popover } from 'bootstrap/dist/js/bootstrap.esm.min.js'
-// import { Toast } from 'bootstrap/dist/js/bootstrap.esm.min.js'
+
 import TheAlertContainer from '@/components/layout/TheAlertContainer.vue'
 import SideBar from '@/components/layout/TheSideBar.vue'
+import TheToastContainer from './components/layout/TheToastContainer.vue'
 
 export default {
-  components: { TheAlertContainer, SideBar },
+  components: { TheAlertContainer, SideBar, TheToastContainer },
   mounted() {
     // Set app theme
     document.body.setAttribute('data-bs-theme', this.$store.getters.getTheme)
@@ -35,8 +37,6 @@ export default {
     Array.from(document.querySelectorAll('button[data-bs-toggle="popover"]')).forEach(
       (popoverNode) => new Popover(popoverNode)
     )
-    //init toast
-    Array.from(document.querySelectorAll('.toast')).forEach((toastNode) => new Toast(toastNode))
   },
   methods: {
     themeChange(val) {
