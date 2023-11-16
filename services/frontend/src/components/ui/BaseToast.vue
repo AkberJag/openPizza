14e47dee4d6c6915f7dad88172d7dce5d576b6d5
AkberJag <akberjag@gmail.com>
2023-11-16 00:06:34 +0000

    :sparkles: feat(component): Added base toast component

---
+++ services/frontend/src/components/ui/BaseToast.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" :id="id">
+    <div class="toast-header">
+      <img class="rounded me-2" alt="🍕" :src="toastImgURL" />
+      <strong class="me-auto">{{ toastTitle }}</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">{{ toastBody }}</div>
+  </div>
+</template>
+
+<script>
+import { Toast } from 'bootstrap/dist/js/bootstrap.esm.min.js'
+export default {
+  props: ['id', 'toastTitle', 'toastBody', 'toastImgURL'],
+  data() {
+    return {
+      toast: null
+    }
+  },
+  mounted() {
+    this.toast = Toast.getOrCreateInstance('#' + this.id)
+    this.toast.autohide = false
+    setTimeout(() => {
+      this.toast.show()
+    }, 1000)
+  },
+  methods: {
+    closeToast() {
+      this.toast.hide()
+    }
+  }
+}
+</script>
