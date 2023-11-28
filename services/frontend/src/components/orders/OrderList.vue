e44914116b279d51efc7515a2ac21d3316e6200f
AkberJag <akberjag@gmail.com>
2023-11-28 00:15:31 +0000

    :sparkles: feat(components/orders): Added order list card header

--- services/frontend/src/components/orders/OrderList.vue
+++ services/frontend/src/components/orders/OrderList.vue
@@ -1,3 +1,50 @@
 <template>
-  <div class="card shadow d-flex"></div>
+  <div class="card shadow d-flex">
+    <!-- card header -->
+    <div class="card-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center fw-bold fs-4">
+            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio1"
+                autocomplete="off"
+                checked
+              />
+              <label class="btn btn-outline-secondary" for="btnradio1">New</label>
+
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio2"
+                autocomplete="off"
+              />
+              <label class="btn btn-outline-secondary" for="btnradio2">Scheduled</label>
+
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio3"
+                autocomplete="off"
+              />
+              <label class="btn btn-outline-secondary" for="btnradio3">In Progress</label>
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio4"
+                autocomplete="off"
+              />
+              <label class="btn btn-outline-secondary" for="btnradio4">Complete</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
